9e9e2735e50b1afc1e2998b8938a3ad25a49aef9
Javac-g <denysov.maxym@gmail.com>
2024-07-23 17:56:12 +0300

    Initially generated setup

---
+++ test/java/com/denisov/SpringSecurityTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denisov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
